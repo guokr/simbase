407f2c527282fb90f82bcd35b64b9efe0db94f4b
zjjott <zjjott@126.com>
2013-08-05 17:24:01 +0800

    some fix

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
     :dependencies [[org.clojure/clojure "1.5.1"]
                    [com.taoensso/carmine "2.0.0"]
                    [net.sf.trove4j/trove4j "3.0.3"]
-                   [com.esotericsoftware.kryo/kryo "2.21"]];;TODO:add org.slf4j and org.quartz
+                   [com.esotericsoftware.kryo/kryo "2.21"]];;TODO:add org.slf4j 
 ;    :dev-dependencies [[net.sf.trove4j/trove4j "3.0.3" :classifier "sources"]]
     :source-paths ["src/main/clojure" "trove"]
     :java-source-paths ["src/main/java"]
