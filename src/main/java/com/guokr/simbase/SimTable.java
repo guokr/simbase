a5d039b93f801a8793c61c0581d97eac14230ea8
zjjott <zjjott@126.com>
2013-08-12 09:53:40 +0800

    merge from remote

--- src/main/java/com/guokr/simbase/SimTable.java
+++ src/main/java/com/guokr/simbase/SimTable.java
@@ -33,7 +33,10 @@ public class SimTable implements KryoSerializable {
 					public int compare(Map.Entry<Integer, Float> e1,
 							Map.Entry<Integer, Float> e2) {
 						int sgn = (int) Math.signum(e2.getValue() - e1.getValue());
-						return sgn<0?-1:1;
+						if (sgn == 0) {
+							sgn = (int) Math.signum(e2.hashCode() - e1.hashCode());
+						}
+						return sgn;
 					}
 				});
 		sortedEntries.addAll(map.entrySet());
@@ -89,7 +92,7 @@ public class SimTable implements KryoSerializable {
 			probs.add((float) (docid + 1));
 			probs.add(length);
 		}
-		int end = probs.size();//end=2050*n
+		int end = probs.size();
 
 		float score = 0;
 		int base = 0;
@@ -108,7 +111,7 @@ public class SimTable implements KryoSerializable {
 					setscore(docid, (int) val - 1, cosine);
 					setscore((int) val - 1, docid, cosine);
 					score = 0;
-					offset = offset + 1;//此时的offset移动至新的向量概率处
+					offset = offset + 1;
 					base = offset + 1;
 				}
 			}
@@ -133,9 +136,9 @@ public class SimTable implements KryoSerializable {
 			indexer.remove(docid);
 			scores.remove(docid);
 		}
-		indexer.remove(docid);// HashMap里没有这个键了也可以用= =
+		indexer.remove(docid);//HashMap里没有这个键了也可以用= =
 		scores.remove(docid);
-
+		
 	}
 
 	public SortedSet<Map.Entry<Integer, Float>> retrieve(int docid) {
@@ -165,10 +168,8 @@ public class SimTable implements KryoSerializable {
 					peer.indexer.put((int) value - 1, start);
 					peer.probs.add(value);
 					cursor++;
-					if (piter.hasNext()) {
-						peer.probs.add(piter.next());
-						cursor++;
-					}
+					peer.probs.add(piter.next());
+					cursor++;
 					start = cursor;
 				} else {
 					peer.probs.add(value);
@@ -260,6 +261,5 @@ public class SimTable implements KryoSerializable {
 			}
 		}
 		logger.info("Save finish");
-	}
-
+    }
 }
