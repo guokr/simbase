e8f11a3e7da5ed5509199435dba4c3ff825e1414
zjjott <zjjott@126.com>
2013-08-13 15:36:39 +0800

    modifity:before SimTable unittest move to new file,so I delete corresponding code in SimEnigneTests.java

--- src/tests/java/com/guokr/simbase/tests/SimEngineTests.java
+++ src/tests/java/com/guokr/simbase/tests/SimEngineTests.java
@@ -24,16 +24,6 @@ public class SimEngineTests {
 		return docids;
 	}
 
-	private void validator(SortedSet<Entry<Integer, Float>> set) {
-		Iterator<Entry<Integer, Float>> iter = set.iterator();
-		float beforeValue = 1.0f;
-		while (iter.hasNext()) {
-			float thisValue = iter.next().getValue();
-			assertTrue(beforeValue >= thisValue);
-			beforeValue = thisValue;
-		}
-	}
-
 	private void delay(int second) {
 
 		try {
@@ -60,60 +50,7 @@ public class SimEngineTests {
 		}
 		System.out.println();
 	}
-	
-	private SimTable initTable() {
-		// 测试用引擎
-		SimTable engine = new SimTable();
-		engine.add(0, new float[] { 0.18257418583505536f, 0.3651483716701107f,
-				0.5477225575051661f, 0.7302967433402214f });
-		engine.add(1, new float[] { 0.18257418583505536f, 0.3651483716701107f,
-				0.7302967433402214f, 0.5477225575051661f });
-		engine.add(2, new float[] { 0.18257418583505536f, 0.5477225575051661f,
-				0.3651483716701107f, 0.7302967433402214f });
-		engine.add(3, new float[] { 0.18257418583505536f, 0.5477225575051661f,
-				0.7302967433402214f, 0.3651483716701107f });
-		engine.add(4, new float[] { 0.18257418583505536f, 0.7302967433402214f,
-				0.3651483716701107f, 0.5477225575051661f });
-		engine.add(5, new float[] { 0.18257418583505536f, 0.7302967433402214f,
-				0.5477225575051661f, 0.3651483716701107f });
-		engine.add(6, new float[] { 0.3651483716701107f, 0.18257418583505536f,
-				0.5477225575051661f, 0.7302967433402214f });
-		engine.add(7, new float[] { 0.3651483716701107f, 0.18257418583505536f,
-				0.7302967433402214f, 0.5477225575051661f });
-		engine.add(8, new float[] { 0.3651483716701107f, 0.5477225575051661f,
-				0.18257418583505536f, 0.7302967433402214f });
-		engine.add(9, new float[] { 0.3651483716701107f, 0.5477225575051661f,
-				0.7302967433402214f, 0.18257418583505536f });
-		engine.add(10, new float[] { 0.3651483716701107f, 0.7302967433402214f,
-				0.18257418583505536f, 0.5477225575051661f });
-		engine.add(11, new float[] { 0.3651483716701107f, 0.7302967433402214f,
-				0.5477225575051661f, 0.18257418583505536f });
-		engine.add(12, new float[] { 0.5477225575051661f, 0.18257418583505536f,
-				0.3651483716701107f, 0.7302967433402214f });
-		engine.add(13, new float[] { 0.5477225575051661f, 0.18257418583505536f,
-				0.7302967433402214f, 0.3651483716701107f });
-		engine.add(14, new float[] { 0.5477225575051661f, 0.3651483716701107f,
-				0.18257418583505536f, 0.7302967433402214f });
-		engine.add(15, new float[] { 0.5477225575051661f, 0.3651483716701107f,
-				0.7302967433402214f, 0.18257418583505536f });
-		engine.add(16, new float[] { 0.5477225575051661f, 0.7302967433402214f,
-				0.18257418583505536f, 0.3651483716701107f });
-		engine.add(17, new float[] { 0.5477225575051661f, 0.7302967433402214f,
-				0.3651483716701107f, 0.18257418583505536f });
-		engine.add(18, new float[] { 0.7302967433402214f, 0.18257418583505536f,
-				0.3651483716701107f, 0.5477225575051661f });
-		engine.add(19, new float[] { 0.7302967433402214f, 0.18257418583505536f,
-				0.5477225575051661f, 0.3651483716701107f });
-		engine.add(20, new float[] { 0.7302967433402214f, 0.3651483716701107f,
-				0.18257418583505536f, 0.5477225575051661f });
-		engine.add(21, new float[] { 0.7302967433402214f, 0.3651483716701107f,
-				0.5477225575051661f, 0.18257418583505536f });
-		engine.add(22, new float[] { 0.7302967433402214f, 0.5477225575051661f,
-				0.18257418583505536f, 0.3651483716701107f });
-		engine.add(23, new float[] { 0.7302967433402214f, 0.5477225575051661f,
-				0.3651483716701107f, 0.18257418583505536f });
-		return engine;
-	}
+
 
 	private SimBase initSimBase(String key) {
 		SimBase simbase = new SimBase();
@@ -187,89 +124,7 @@ public class SimEngineTests {
 
 	}
 
-	@Test
-	public void test() {
-		SimTable engine = new SimTable();
-		engine.add(2, new float[] { 0.9f, 0.1f, 0f });
-		engine.add(3, new float[] { 0.9f, 0f, 0.1f });
-		engine.add(5, new float[] { 0.1f, 0.9f, 0f });
-		engine.add(7, new float[] { 0.1f, 0f, 0.9f });
-		engine.add(11, new float[] { 0f, 0.9f, 0.1f });
-		engine.add(13, new float[] { 0f, 0.1f, 0.9f });
-		// System.out.println(engine.retrieve(2));
-		// System.out.println(engine.retrieve(3));
-		// System.out.println(engine.retrieve(5));
-		// System.out.println(engine.retrieve(7));
-		// System.out.println(engine.retrieve(11));
-		// System.out.println(engine.retrieve(13));
 
-		assertTrue((int) (1000 * engine.similarity(2, 3)) == (int) (1000 * engine
-				.similarity(5, 11)));
-		assertTrue((int) (1000 * engine.similarity(2, 3)) == (int) (1000 * engine
-				.similarity(7, 13)));
-		assertTrue((int) (1000 * engine.similarity(2, 5)) == (int) (1000 * engine
-				.similarity(3, 7)));
-		assertTrue((int) (1000 * engine.similarity(2, 5)) == (int) (1000 * engine
-				.similarity(11, 13)));
-		assertTrue((int) (1000 * engine.similarity(2, 7)) == (int) (1000 * engine
-				.similarity(3, 5)));
-		assertTrue((int) (1000 * engine.similarity(2, 7)) == (int) (1000 * engine
-				.similarity(7, 11)));
-		assertTrue((int) (1000 * engine.similarity(2, 11)) == (int) (1000 * engine
-				.similarity(3, 13)));
-		assertTrue((int) (1000 * engine.similarity(2, 13)) == (int) (1000 * engine
-				.similarity(5, 7)));
-
-		engine.add(2, new float[] { 0f, 0.1f, 0.9f });
-		engine.add(3, new float[] { 0.1f, 0f, 0.9f });
-		engine.add(5, new float[] { 0f, 0.9f, 0.1f });
-		engine.add(7, new float[] { 0.9f, 0f, 0.1f });
-		engine.add(11, new float[] { 0.1f, 0.9f, 0f });
-		engine.add(13, new float[] { 0.9f, 0.1f, 0f });
-
-		// System.out.println(engine.retrieve(2));
-		// System.out.println(engine.retrieve(3));
-		// System.out.println(engine.retrieve(5));
-		// System.out.println(engine.retrieve(7));
-		// System.out.println(engine.retrieve(11));
-		// System.out.println(engine.retrieve(13));
-
-		assertTrue((int) (1000 * engine.similarity(2, 3)) == (int) (1000 * engine
-				.similarity(5, 11)));
-		assertTrue((int) (1000 * engine.similarity(2, 3)) == (int) (1000 * engine
-				.similarity(7, 13)));
-		assertTrue((int) (1000 * engine.similarity(2, 5)) == (int) (1000 * engine
-				.similarity(3, 7)));
-		assertTrue((int) (1000 * engine.similarity(2, 5)) == (int) (1000 * engine
-				.similarity(11, 13)));
-		assertTrue((int) (1000 * engine.similarity(2, 7)) == (int) (1000 * engine
-				.similarity(3, 5)));
-		assertTrue((int) (1000 * engine.similarity(2, 7)) == (int) (1000 * engine
-				.similarity(7, 11)));
-		assertTrue((int) (1000 * engine.similarity(2, 11)) == (int) (1000 * engine
-				.similarity(3, 13)));
-		assertTrue((int) (1000 * engine.similarity(2, 13)) == (int) (1000 * engine
-				.similarity(5, 7)));
-
-	}
-
-	@Test
-	public void test2() {
-		SimTable engine = initTable();
-		int count = 0;
-		while (count < 24) {
-			// System.out.println(engine.retrieve(count));
-			count++;
-		}
-		assertTrue((int) (1000 * engine.similarity(0, 1)) == (int) (1000 * engine
-				.similarity(2, 4)));
-		count = 0;
-		while (count < 24) {
-			SortedSet<Entry<Integer, Float>> result = engine.retrieve(count);
-			validator(result);
-			count++;
-		}
-	}
 
 	@Test
 	public void test_del_clean() {
