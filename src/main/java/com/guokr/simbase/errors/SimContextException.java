b2b08c12001d283df5ee84e3d36e2f6746eb391e
Mingli Yuan <mingli.yuan@gmail.com>
2013-12-16 16:46:11 +0800

    add context exception

---
+++ src/main/java/com/guokr/simbase/errors/SimContextException.java
@@ -0,0 +1,19 @@
+package com.guokr.simbase.errors;
+
+public class SimContextException extends RuntimeException {
+
+    private static final long serialVersionUID = 4295837255563564513L;
+
+    public SimContextException(String msg) {
+        super(msg);
+    }
+
+    public SimContextException(Throwable t) {
+        super(t);
+    }
+
+    public SimContextException(String msg, Throwable t) {
+        super(msg, t);
+    }
+
+}
