e25a08040d9162e6df2d3f9dcfb0b125623eeea0
Mingli Yuan <mingli.yuan@gmail.com>
2013-12-25 04:47:54 +0800

    rm unused field

--- src/main/java/com/guokr/simbase/errors/SimException.java
+++ src/main/java/com/guokr/simbase/errors/SimException.java
@@ -1,22 +1,19 @@
 package com.guokr.simbase.errors;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class SimException extends RuntimeException {
 
-    private static final long   serialVersionUID = -4170061639543762537L;
-    private Map<String, String> context;
+	private static final long serialVersionUID = -4170061639543762537L;
 
-    public SimException(String msg) {
-        super(msg);
-    }
+	public SimException(String msg) {
+		super(msg);
+	}
 
-    public SimException(Throwable t) {
-        super(t);
-    }
+	public SimException(Throwable t) {
+		super(t);
+	}
 
-    public SimException(String msg, Throwable t) {
-        super(msg, t);
-    }
+	public SimException(String msg, Throwable t) {
+		super(msg, t);
+	}
 }
