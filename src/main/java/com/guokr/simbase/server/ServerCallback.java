13873b7176d7fa5b34bf8aa737bfe7ee0eb14ace
Mingli Yuan <mingli.yuan@gmail.com>
2014-03-05 15:54:32 +0800

    solve inconsistency by using concurrency counter

--- src/main/java/com/guokr/simbase/server/ServerCallback.java
+++ src/main/java/com/guokr/simbase/server/ServerCallback.java
@@ -1,27 +1,28 @@
 package com.guokr.simbase.server;
 
 import java.nio.ByteBuffer;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import com.guokr.simbase.SimCallback;
 
 public class ServerCallback extends SimCallback {
 
-    public RespCallback response;
-    public ByteBuffer[] buffers;
+    public RespCallback   response;
+    public ByteBuffer[]   buffers;
 
-    private int         pipesize;
+    private AtomicInteger pipesize;
 
     public ServerCallback(int size, RespCallback cb) {
         this.buffers = new ByteBuffer[size];
         this.response = cb;
-        this.pipesize = size;
+        this.pipesize = new AtomicInteger(size);
     }
 
     public void response() {
-        if (pipesize == 1) {
+        if (pipesize.intValue() == 1) {
             response.run(buffers);
         } else {
-            pipesize--;
+            pipesize.decrementAndGet();
         }
     }
 
